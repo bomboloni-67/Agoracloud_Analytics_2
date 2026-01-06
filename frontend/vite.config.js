715aac63914f8aa51e34018378f67ee3454a7d4e
Jayden <babyzdriever@example.com>
2026-01-06 10:58:27 +0800

    Corrected the base for vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/bomboloni-67/Agoracloud_Analytics_2',
+  base: '/Agoracloud_Analytics_2/',
 })
